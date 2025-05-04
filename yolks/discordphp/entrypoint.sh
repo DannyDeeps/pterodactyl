7a934d99c5dc22dfca95c29292910f791451f23c
Danny Huggins <dannyhuggins@proton.me>
2025-05-04 13:55:49 +0100

    direct repo clone in image

--- yolks/discordphp/entrypoint.sh
+++ yolks/discordphp/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 cd /home/container
 
-# Output Composer version
-composer -V || { echo "[Composer] Error: Composer not detected."; exit 1; }
-
-# Output PHP version
-php -v || { echo "[PHP] Error: PHP not detected."; exit 1; }
-
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
